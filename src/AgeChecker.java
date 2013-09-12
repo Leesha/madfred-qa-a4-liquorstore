ec2ce81de4a84c3ccff3dd6e91c549ad5a4f92b0
Leesha <student@192.168.0.22>
2013-09-12 10:30:27 -0400

    here is my asg 4

--- src/AgeChecker.java
+++ src/AgeChecker.java
@@ -1,15 +1,33 @@
+import javax.swing.JOptionPane;
+
 
 public class AgeChecker {
-
+//this creates the dialog box and prompts information from the user
 	public static void main(String[] args) {
-		/*---------------------------------
-		 * In this file, you will recreate 
-		 * the age verification program we
-		 * created in class. When you're
-		 * finished, delete these instructions
-		 * before submitting.
-		 ---------------------------------*/
-
-	}
+		String firstName = JOptionPane.showInputDialog(null, "What is your first name?");
+		String lastName = JOptionPane.showInputDialog(null, "What is your last name?");
+		int yearOfBirth = Integer.parseInt(JOptionPane.showInputDialog(null, "what year were you born?"));
+// these are the variables that hold the information		
+		boolean oldEnough = verifyLegalAge(yearOfBirth);
+		String fullName = concatenateNames(firstName, lastName);
+//this verifies the information recieved whehter it is tru or false 	
+		if (oldEnough == true){
+			JOptionPane.showMessageDialog(null,fullName + " you may proceed with your shopping");
+		}
+		else{
+			JOptionPane.showMessageDialog(null, fullName + "you're not old enough, try disney.com instead!");
+		}
+		}
+// this shows output corresponding to the varification of age
+		public static boolean verifyLegalAge(int yob){
+			boolean legalAgeStatus = (2013-yob) >= 19;
+			return legalAgeStatus;
+		}	
+// this strings the names together so i dont have to call both variables.		
+			public static String concatenateNames(String fn, String ln){
+				String fullName = fn + " " + ln;
+				return fullName;
+		}
+	
 
 }
